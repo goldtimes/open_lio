5887b8378cca46fec4978cafb01136b001f5a1ff
lihang <1019825699@qq.com>
2024-09-29 16:14:52 +0000

    "增加传感器数据结构"

--- src/sensors/imu.hh
+++ src/sensors/imu.hh
@@ -3,17 +3,18 @@
 #include <Eigen/Core>
 #include <Eigen/Dense>
 #include <memory>
+#include "sensors/data.hh"
 namespace lio {
-class IMU {
-   public:
-    IMU() = default;
-    IMU(double time, const Eigen::Vector3d& gyro, const Eigen::Vector3d& acc)
-        : timestamp_(time), acc_(acc), gyro_(gyro) {
+struct IMUData : DataBase {
+    EIGEN_MAKE_ALIGNED_OPERATOR_NEW
+    IMUData() = default;
+    IMUData(double time, const Eigen::Vector3d& gyro, const Eigen::Vector3d& acc)
+        : DataBase(time), acc_(acc), gyro_(gyro) {
     }
-    double timestamp_;
     Eigen::Vector3d acc_;
     Eigen::Vector3d gyro_;
+    Eigen::Quaterniond orientation_;
 };
 }  // namespace lio
 
-using IMUPtr = std::shared_ptr<lio::IMU>;
+using IMUPtr = std::shared_ptr<lio::IMUData>;
--- src/sensors/odom.hh
+++ src/sensors/odom.hh
@@ -3,16 +3,17 @@
 #include <Eigen/Core>
 #include <Eigen/Dense>
 #include <memory>
+#include "sensors/data.hh"
 
 namespace lio {
-class Odom {
+struct Odom : DataBase {
    public:
     Odom() = default;
     Odom(double time, double left_pulse, double right_pulse)
-        : timestamp_(time), left_pulse_(left_pulse), right_pulse_(right_pulse) {
+        : DataBase(time), left_pulse_(left_pulse), right_pulse_(right_pulse) {
     }
-    double timestamp_ = 0.0;
     double left_pulse_ = 0.0;
     double right_pulse_ = 0.0;
 };
 }  // namespace lio
+using OdomPtr = std::shared_ptr<lio::Odom>;
